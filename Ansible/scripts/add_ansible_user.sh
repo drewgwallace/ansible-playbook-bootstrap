48a0e7c42951c00e9aabf214cdea3d63765a63c8
Drew G. Wallace <32915923+drewgwallace@users.noreply.github.com>
2017-10-26 16:07:12 -0700

    Updated null client configuration, playbook, script

--- Ansible/scripts/add_ansible_user.sh
+++ Ansible/scripts/add_ansible_user.sh
@@ -3,7 +3,7 @@
 echo "This is a bootstrap to add an Ansible user and configure ssh key-based logins"
 echo
 
-#Prompt for taget machine
+#Prompt for target machine
 echo -e "Please provide target machine: \c"
 read target
 echo
@@ -24,8 +24,8 @@ echo
 echo "Attempting..."
 echo
 
-#echo $target
-#echo $username
-#echo $ansible_pass
+# echo $target
+# echo $username
+# echo $ansible_pass
 
 ansible-playbook /etc/ansible/playbooks/add_ansible_user.yaml -e "ansible_user=$username ansible_ssh_pass=$user_pass ansible_become_pass=$user_pass" --extra-vars=" host=$target ansible_pass=$ansible_pass" -vv
